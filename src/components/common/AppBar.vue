ca4ddf8a4167bb759bf0fb0a988464d035ab306f
yuzin9712 <dbwls97@ajou.ac.kr>
2021-10-19 15:54:40 +0900

    FE:헤더/푸터 등록

---
+++ src/components/common/AppBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app-bar app fixed color="#EFB775">
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+    <v-toolbar-title>KB 스타Fit</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<style scoped>
+* {
+  color: white;
+}
+</style>
---
+++ src/components/home/NameCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card color="#EFB775">
+    <v-row no-gutters>
+      <v-col cols="6" sm="6" align-self="center">
+        <v-avatar size="50%" class="ma-3">
+          <img  src="../../assets/점핑아거.png" alt="캐릭터"/>
+        </v-avatar>
+      </v-col>
+      <v-col cols="6" sm="6" align-self="center">
+        <v-card-title class="pa-0">
+          <p class="body-1 font-weight-black">마안재</p>
+          <p class="caption">님</p>
+        </v-card-title>
+        <v-card-subtitle class="pa-0 caption font-weight-thin">오늘도 건강한 하루 보내세요🏋🏻‍♀️</v-card-subtitle>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
