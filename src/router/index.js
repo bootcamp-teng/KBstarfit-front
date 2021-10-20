a25659d1a6a0565f6df45120f0a57dfd535dc7c2
jongeunShin95 <jongeunshin95@gmail.com>
2021-10-20 17:33:59 +0900

    목표 설정 화면 임시 완료

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import SetGoal from "../views/SetGoal.vue";
 
 Vue.use(VueRouter);
 
@@ -19,6 +20,11 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/setGoal",
+    name: "SetGoal",
+    component: SetGoal,
+  }
 ];
 
 const router = new VueRouter({
---
+++ src/views/SetGoal.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <set-goal-body></set-goal-body>
+  <reward-description></reward-description>
+  <scrol-list-box></scrol-list-box>
+  <goal-button></goal-button>
+</div>
+</template>
+
+<script>
+import SetGoalBody from '../components/goal/SetGoalBody.vue';
+import RewardDescription from '../components/goal/RewardDescription.vue';
+import GoalButton from '../components/goal/GoalButton.vue';
+import ScrolListBox from '../components/goal/ScrolListBox.vue';
+
+export default {
+  name: "Home",
+
+  components: {
+    SetGoalBody,
+    RewardDescription,
+    GoalButton,
+    ScrolListBox,
+  },
+};
+</script>
