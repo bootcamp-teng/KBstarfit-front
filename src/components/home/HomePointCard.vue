cdf489ce52429e3d46aca392d3719eee6df197f6
DESKTOP-THVMA8D\Admin <coconamy77@gmail.com>
2021-10-22 14:54:05 +0900

    포인트페이지 이동

--- src/components/home/HomePointCard.vue
+++ src/components/home/HomePointCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-2">
     <span class="body-2 ml-5 mb-0">나의 포인트 💸</span>
-    <v-card elevation="5" class="pl-3 ml-5 mr-5 mt-2" color="#6782D4" dark>
+    <v-card elevation="5" class="pl-3 ml-5 mr-5 mt-2" color="#6782D4" dark @click="pushPoint">
       <v-row no-gutters>
         <v-col cols="6" align-self="center" align="center">
           <img width="50%" src="../../assets/물아거.png" alt="캐릭터" />
@@ -28,7 +28,10 @@ export default {
         ...mapGetters(['point'])
     },
     methods: {
-        ...mapActions(['getUserPoint'])
+        ...mapActions(['getUserPoint']),
+        pushPoint: function() {
+            this.$router.push({name: 'Point'});
+        }
     },
     filters: {
         userPoint: function(point) {
