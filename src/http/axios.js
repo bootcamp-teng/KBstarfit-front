41630858d917662f51301b12dafdabd67d35f66b
yuzin9712 <dbwls97@ajou.ac.kr>
2021-10-25 14:26:08 +0900

    FE: 리프레시 토큰으로 액세스 토큰 업데이트

--- src/http/axios.js
+++ src/http/axios.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
-// const client_id = '515071488860-s2aoiepptpuhc5tkj18qj763iq1b1lf9.apps.googleusercontent.com';
-// const client_secret = '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';
-// const grant_type = 'refresh_token';
-// var refresh_token = getCookie('fit-refresh');
+const client_id = '515071488860-s2aoiepptpuhc5tkj18qj763iq1b1lf9.apps.googleusercontent.com';
+const client_secret = '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';
+const grant_type = 'refresh_token';
+
 
 axios.interceptors.response.use(function(res) {
     console.log('response interceptor!!!!');
     return res;
-}, function(err) {
+}, async function(err) {
     console.log('err response' + err)
     const {
+        config,
         response: {status}
     } = err;
 
@@ -18,9 +19,20 @@ axios.interceptors.response.use(function(res) {
     401 보내주는 건 google 밖에 없음!
     */
     if(status == 401) {
-        console.log(401);
-        // await axios.post('https://www.googleapis.com/oauth2/v4/token')
-        console.log(getCookie('fit-refresh'));
+        console.log('401 토큰 에러');
+
+        const originalRequest = config;
+        var refresh_token = await getCookie('fit-refresh');
+
+        const {data} = await axios.post(`https://www.googleapis.com/oauth2/v4/token?client_id=${client_id}&client_secret=${client_secret}&refresh_token=${refresh_token}&grant_type=${grant_type}`);
+
+        document.cookie = `fit-access=${data['access_token']}`;
+        
+        console.log(originalRequest);
+        originalRequest.headers.Authorization = `Bearer ${data['access_token']}`;
+
+        return axios(originalRequest);
+
     }
     return Promise.reject(err);
 });
