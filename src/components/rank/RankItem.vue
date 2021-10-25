ac49171176397f5115d21538d8c2a2266e995737
jongeunShin95 <jongeunshin95@gmail.com>
2021-10-25 14:48:37 +0900

    FE: 1,2,3 등 표시

--- src/components/rank/RankItem.vue
+++ src/components/rank/RankItem.vue
@@ -1,6 +1,11 @@
 <template>
   <v-card hover elevation="5" class="pl-3 ml-5 mr-5 mt-2" color="#EF7880" dark>
-    <v-card-title class="body-1"> {{ user.username }} </v-card-title>
+    <v-card-title class="body-1"> 
+      {{ user.username }} 
+      <v-icon v-if="user.id === 1" color="#FFD700">military_tech</v-icon>
+      <v-icon v-if="user.id === 2" color="#C0C0C0">military_tech</v-icon>
+      <v-icon v-if="user.id === 3" color="#800000">military_tech</v-icon>
+    </v-card-title>
     <v-card-subtitle class="body-2 pb-2">
       {{ user.exer }}
     </v-card-subtitle>
