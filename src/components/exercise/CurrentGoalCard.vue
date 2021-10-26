1e40bdbdedbf3d5fd8a4f01774b342cb2ba7fcfc
YJcheon <35832665+YJcheon@users.noreply.github.com>
2021-10-26 11:05:09 +0900

    exercise_Record_commit

---
+++ src/components/exercise/CurrentGoalCard.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="spacing-playground ma-4">
+    <v-card  color="#DEB887"  dark>
+      <v-card-title class="spacing-playground pb-1">
+        <p class="text-h6 spacing-playground mb-1">마안재님의 선택 목표:</p>
+        <p class="text-h4 spacing-playground mb-0">{{this.title}}</p>
+      </v-card-title>
+      <v-card-actions>        
+      </v-card-actions>
+    </v-card>
+ 
+  <v-card v-if="this.status==0" color="#EF7880"  dark>
+      <v-card-title class="spacing-playground pb-1">
+        <p class="text-h6 spacing-playground mb-1">목표 운동량: </p>
+        <p class="text-h4 spacing-playground mb-0">{{this.total_amount}}보</p>
+        <p class="text-h6 spacing-playground mb-1">현재 운동량: </p>
+        <p class="text-h4 spacing-playground mb-0">40000보</p>
+        <p class="text-h4 spacing-playground mb-0">진행중!</p>
+      </v-card-title>
+      <v-card-actions>
+      </v-card-actions>
+    </v-card>
+  <v-card v-else-if="this.status==1" color="#1E90FF"  dark>
+      <v-card-title class="spacing-playground pb-1">
+        <p class="text-h6 spacing-playground mb-1">목표 운동량: </p>
+        <p class="text-h4 spacing-playground mb-0">{{this.total_amount}}보</p>
+        <p class="text-h6 spacing-playground mb-1">현재 운동량: </p>
+        <p class="text-h4 spacing-playground mb-0">40000보</p>
+        <p class="text-h4 spacing-playground mb-0">달성!</p>
+      </v-card-title>
+      <v-card-actions>
+      </v-card-actions>
+    </v-card>
+    <v-card v-else color="#696969"  dark>
+      <v-card-title class="spacing-playground pb-1">
+        <p class="text-h6 spacing-playground mb-1">목표 운동량: </p>
+        <p class="text-h4 spacing-playground mb-0">{{this.total_amount}}보</p>
+        <p class="text-h6 spacing-playground mb-1">현재 운동량: </p>
+        <p class="text-h4 spacing-playground mb-0">40000보</p>
+        <p class="text-h4 spacing-playground mb-0">포기...</p>
+
+      </v-card-title>
+      <v-card-actions>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+  export default {
+    data (){
+      
+      return {
+      title : "",
+      pageNum: this.$route.params.id,
+      total_amount :0,
+      current_amount: 0,
+      status:2,
+      current_run : 0,
+    };
+  },
+  methods: {
+    fetchData(pageNum) {
+      axios
+        .get("http://teng.169.56.174.139.nip.io/starfitgoal/v1/usergoal/" + pageNum)
+        .then(res => {
+          this.title= res.data.title;
+          this.total_amount = res.data.dayExerAmt * res.data.period;
+          this.status = res.data.statusCode;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  },
+  mounted() {
+    this.fetchData(this.pageNum);
+  }}
+</script>
---
+++ src/components/exercise/ExerciseHistoryCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="spacing-playground ma-4">
+    <p class="text-h5 font-weight-black pt-3">운동 내역🎫</p>
+    <v-card
+      color="#98FB98"
+    >
+      <table>
+        <tr 
+          v-for="(item, i) in this.items.slice().reverse()"
+          :key="i"
+        >
+          <td class="font-weight-black" v-show="i>=0 || item.date!=items[i-1].date" v-text="item.date">
+          </td>
+          <td v-text="item.desc">
+          </td>
+          <td v-if="item.point>0">
+          </td>
+          <td v-else>
+            
+          </td>
+          <td class="font-weight-black" style="color: #6782D4" v-if="item.point>0" v-text="' + '+item.point+' 보' ">
+          </td>
+          <td class="font-weight-black" style="color: #EF7880" v-else v-text="item.point+' 보'" >
+          </td>
+          <td class="font-weight-black" style="color: #000000" v-text="'달성 운동량 : ' +(current_run+=item.point)+ ' 보'">
+          </td>
+        </tr>
+      </table>  
+    </v-card>
+        <v-btn
+      color="primary"
+      class="mt-2"
+      dark
+      @click=onClickRedirect()
+    >
+      뒤로가기
+    </v-btn>
+  </div>
+</template>
+<script>
+  export default {
+    data ()
+    {return{
+      pageNum: this.$route.params.id,
+      items: [
+        {
+          date: '10.20',
+          desc: '걷기',
+          point: 8000
+        },
+        {
+          date: '10.19',
+          desc: '걷기',
+          point: 8000
+        },{
+          date: '10.18',
+          desc: '걷기',
+          point: 8000
+        },{
+          date: '10.16',
+          desc: '미션 시작',
+          point: 0
+        },
+      ],
+      current_run : 0,
+    }},
+    methods :{
+      onClickRedirect: function () {   
+          history.back();
+      }
+    }
+  }
+</script>
